c338b6e3faf4a54b6a13203bf0400aa263770564
Nate Richman <nate@nrichman.dev>
2019-07-22 18:57:09 -0500

    Added update to vpn.nrichman.dev A record

--- update.js
+++ update.js
@@ -2,7 +2,7 @@ var https = require('https');
 var cp = require('child_process');
 var credentials = require('./credentials.js');
 var IP = cp.execSync('curl -s -X GET "https://myip.dnsomatic.com"');	 
-var options = {
+var options1 = {
   host: 'api.cloudflare.com',
   port: '443',
   path: '/client/v4/zones/'+credentials.zone+'/dns_records/'+credentials.dns_record,
@@ -13,19 +13,44 @@ var options = {
 		"Content-Type": "application/json",
 	}
 }
-const req = https.request(options, (res) => {
+var options2 = {
+  host: 'api.cloudflare.com',
+  port: '443',
+  path: '/client/v4/zones/'+credentials.zone+'/dns_records/'+credentials.dns_record_vpn,
+  method: 'PUT',
+	headers: {
+		"X-Auth-Email": credentials.username,
+		"X-Auth-Key": credentials.api_key,
+		"Content-Type": "application/json",
+	}
+}
+const req1 = https.request(options1, (res) => {
+	res.on('data', (d) => {
+		process.stdout.write(d);
+	});
+});
+const req2 = https.request(options2, (res) => {
 	res.on('data', (d) => {
 		process.stdout.write(d);
 	});
 });
 //process.stdout.write(IP);
-var data = {
+var data1 = {
 	type: "A",
 	name: "nrichman.dev",
 	content: IP.toString(),
 	ttl: 3600,
 	proxied: false
 }
+var data2 = {
+	type: "A",
+	name: "vpn.nrichman.dev",
+	content: IP.toString(),
+	ttl: 3600,
+	proxied: false
+}
 //process.stdout.write(JSON.stringify(data));
-req.write(JSON.stringify(data));
-req.end();
+req1.write(JSON.stringify(data1));
+req1.end();
+req2.write(JSON.stringify(data2));
+req2.end();
